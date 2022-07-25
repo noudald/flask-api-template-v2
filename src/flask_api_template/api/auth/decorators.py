655252bb49d442ad463902b8426ef106832513aa
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-25 07:24:19 +0200

    Add decorators for token and admin access

---
+++ src/flask_api_template/api/auth/decorators.py
@@ -0,0 +1,48 @@
+from functools import wraps
+
+from flask import request
+from werkzeug.exceptions import Forbidden, Unauthorized
+
+from flask_api_template.models.user import User
+
+
+def _check_access_token(admin_only):
+    token = request.headers.get('Authorization')
+
+    if not token:
+        raise Unauthorized()
+
+    result = User.decode_access_token(token)
+    if result.failure:
+        raise Unauthorized()
+
+    return result.value
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token_payload = _check_access_token(admin_only=False)
+
+        for name, val in token_payload.items():
+            setattr(decorated, name, val)
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+def admin_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token_payload = _check_access_token(admin_only=True)
+
+        if not token_payload['admin']:
+            raise Forbidden()
+
+        for name, val in token_payload.items():
+            setattr(decorated, name, val)
+
+        return f(*args, **kwargs)
+
+    return decorated
