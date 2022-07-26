db2dbbe8775fbe4b52a9f294fbc85e4793e868a2
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-26 07:14:33 +0200

    Add dto for Todo List example

---
+++ src/flask_api_template/api/todo/dto.py
@@ -0,0 +1,44 @@
+from flask_restx import Model
+from flask_restx import fields, inputs
+from flask_restx.reqparse import RequestParser
+
+
+todo_reqparser = RequestParser(bundle_errors=True)
+todo_reqparser.add_argument(
+    name='assigned',
+    type=str,
+    location='form',
+    required=True,
+    nullable=False,
+)
+todo_reqparser.add_argument(
+    name='task',
+    type=str,
+    location='form',
+    required=True,
+    nullable=False,
+)
+todo_reqparser.add_argument(
+    name='deadline',
+    type=str,
+    location='form',
+    required=True,
+    nullable=False,
+)
+todo_reqparser.add_argument(
+    name='finished',
+    type=bool,
+    location='form',
+    required=True,
+    nullable=False
+)
+
+todo_model = Model(
+    'TodoTask',
+    {
+        'assigned': fields.String,
+        'task': fields.String,
+        'deadline': fields.String,
+        'finished': fields.Boolean,
+    }
+)
