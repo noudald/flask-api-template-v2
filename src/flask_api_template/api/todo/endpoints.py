89060969676f97f56d80cabeae58047ed5aeb580
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-07-27 07:57:00 +0200

    Restructure endpoints for todo tasks

--- src/flask_api_template/api/todo/endpoints.py
+++ src/flask_api_template/api/todo/endpoints.py
@@ -3,9 +3,7 @@ from http import HTTPStatus
 from flask_restx import Namespace, Resource
 
 from flask_api_template.api.todo.dto import (
-    todo_add_reqparser,
-    todo_get_reqparser,
-    todo_update_reqparser,
+    todo_reqparser,
     todo_model
 )
 from flask_api_template.api.todo.business import (
@@ -20,9 +18,9 @@ todo_ns = Namespace(name='todo', validate=True)
 todo_ns.models[todo_model.name] = todo_model
 
 
-@todo_ns.route('/task', endpoint='task')
-class TodoTask(Resource):
-    @todo_ns.expect(todo_add_reqparser)
+@todo_ns.route('/task', endpoint='todo_tasks')
+class TodoList(Resource):
+    @todo_ns.expect(todo_reqparser)
     @todo_ns.response(
         int(HTTPStatus.CREATED),
         'New task was successfully create.'
@@ -37,10 +35,40 @@ class TodoTask(Resource):
     )
     @todo_ns.marshal_with(todo_model)
     def post(self):
-        # New todo task
+        '''Add new task to todo list.'''
         pass
 
-    @todo_ns.expect(todo_get_reqparser)
+    def get(self):
+        '''Get list of all todo list tasks.'''
+        pass
+
+
+@todo_ns.route('/task/<int:id>', endpoint='todo_task')
+class TodoTask(Resource):
+    @todo_ns.expect(todo_reqparser)
+    @todo_ns.response(
+        int(HTTPStatus.ACCEPTED),
+        'Successfully updated task.',
+        todo_model
+    )
+    @todo_ns.response(
+        int(HTTPStatus.NOT_FOUND),
+        'Task not found. Id incorrect.'
+    )
+    @todo_ns.response(
+        int(HTTPStatus.BAD_REQUEST),
+        'Validation error.'
+    )
+    @todo_ns.response(
+        int(HTTPStatus.INTERNAL_SERVER_ERROR),
+        'Internal server error.'
+    )
+    @todo_ns.marshal_with(todo_model)
+    def put(self, id):
+        '''Update todo tasks.'''
+        pass
+
+    @todo_ns.expect(todo_reqparser)
     @todo_ns.response(
         int(HTTPStatus.OK),
         'Successfully retrieved task.',
@@ -60,33 +88,9 @@ class TodoTask(Resource):
     )
     @todo_ns.marshal_with(todo_model)
     def get(self, id):
-        # Get todo task
+        '''Get todo task.'''
         pass
 
-    @todo_ns.expect(todo_update_reqparser)
-    @todo_ns.response(
-        int(HTTPStatus.ACCEPTED),
-        'Successfully updated task.',
-        todo_model
-    )
-    @todo_ns.response(
-        int(HTTPStatus.NOT_FOUND),
-        'Task not found. Id incorrect.'
-    )
-    @todo_ns.response(
-        int(HTTPStatus.BAD_REQUEST),
-        'Validation error.'
-    )
-    @todo_ns.response(
-        int(HTTPStatus.INTERNAL_SERVER_ERROR),
-        'Internal server error.'
-    )
-    @todo_ns.marshal_with(todo_model)
-    def update(self, id):
-        # Update todo task
-        pass
-
-    @todo_ns.expect(todo_get_reqparser)
     @todo_ns.response(
         int(HTTPStatus.NO_CONTENT),
         'Successfully deleted task.'
@@ -104,5 +108,5 @@ class TodoTask(Resource):
         'Internal server error.'
     )
     def delete(self, id):
-        # Delete todo task
+        '''Delete todo task.'''
         pass
