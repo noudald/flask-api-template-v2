df678b3b48687d9076e2f7d71fcc433b3f7735ac
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-08-11 06:43:15 +0200

    Fix wrong import in unit test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,14 @@
 import os
 
 from flask_api_template import create_app
-from flask_api_template.config import SQLITE_DEV, SQLITE_PROD, SQLITE_TEST
+from flask_api_template.config import SQL_DEV, SQL_PROD, SQL_TEST
 
 
 def test_config_development():
     app = create_app('development')
     assert app.config['SECRET_KEY'] != ''
     assert not app.config['TESTING']
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL', SQLITE_DEV)
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL', SQL_DEV)
     assert app.config['TOKEN_EXPIRE_HOURS'] == 0
     assert app.config['TOKEN_EXPIRE_MINUTES'] == 15
 
@@ -17,7 +17,7 @@ def test_config_testing():
     app = create_app('testing')
     assert app.config['SECRET_KEY'] != ''
     assert app.config['TESTING']
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == SQLITE_TEST
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == SQL_TEST
     assert app.config['TOKEN_EXPIRE_HOURS'] == 0
     assert app.config['TOKEN_EXPIRE_MINUTES'] == 0
 
@@ -26,6 +26,6 @@ def test_config_production():
     app = create_app('production')
     assert app.config['SECRET_KEY'] != ''
     assert not app.config['TESTING']
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL', SQLITE_PROD)
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL', SQL_PROD)
     assert app.config['TOKEN_EXPIRE_HOURS'] == 1
     assert app.config['TOKEN_EXPIRE_MINUTES'] == 0
